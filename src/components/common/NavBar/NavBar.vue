0b77e5527191d25e4dc60158ea674a50d3814be3
Arman Amini <armanwithamini@gmail.com>
2024-05-09 05:19:44 -0700

    feat: :sparkles: add NavBar

--- src/components/common/NavBar/NavBar.vue
+++ src/components/common/NavBar/NavBar.vue
@@ -1,3 +1,19 @@
 <template>
-  <div class="text-h1">f</div>
+  <div class="bg-blue flex items-center h-16 px-12 justify-between">
+    <img src="images/logo.svg" />
+    <div class="flex text-h1">
+      <RouterLink class="inline-flex items-center text-white mr-10" to="/">
+        <img src="images/schedule.png" class="h-6 mr-1" />
+        Schedule
+      </RouterLink>
+      <RouterLink class="inline-flex items-center text-white mr-10" to="/leaderboard">
+        <img src="images/leaderboard.png" class="h-6 mr-1" />
+        Leaderboard
+      </RouterLink>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
