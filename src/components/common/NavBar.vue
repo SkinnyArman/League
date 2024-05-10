2dc0f2ddee7cef64e4d0c0de92a5fae544ff7107
Arman Amini <armanwithamini@gmail.com>
2024-05-10 13:02:58 -0700

    feat: :sparkles: remove routes hardcoding

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -2,11 +2,17 @@
   <div class="bg-blue flex items-center h-16 px-12 justify-between">
     <img src="images/logo.svg" />
     <div class="flex text-h1">
-      <RouterLink class="inline-flex items-center text-white mr-10" to="/">
+      <RouterLink
+        class="inline-flex items-center text-white mr-10"
+        :to="RoutesEnum.Schedule"
+      >
         <img src="images/schedule.png" class="h-6 mr-1" />
         Schedule
       </RouterLink>
-      <RouterLink class="inline-flex items-center text-white mr-10" to="/leaderboard">
+      <RouterLink
+        class="inline-flex items-center text-white mr-10"
+        :to="RoutesEnum.Leaderboard"
+      >
         <img src="images/leaderboard.png" class="h-6 mr-1" />
         Leaderboard
       </RouterLink>
@@ -16,4 +22,5 @@
 
 <script setup>
 import { RouterLink } from "vue-router";
+import { RoutesEnum } from "../../router";
 </script>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,23 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from "vue-router";
 
-import SchedulePage from './pages/SchedulePage.vue'
-import LeaderboardPage from './pages/LeaderboardPage.vue'
+import SchedulePage from "./pages/SchedulePage.vue";
+import LeaderboardPage from "./pages/LeaderboardPage.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
-  { path: '/', component: SchedulePage },
-  { path: '/leaderboard', component: LeaderboardPage },
-]
+  { path: "/", component: SchedulePage },
+  { path: "/schedule", component: SchedulePage, name: "Schedule" },
+  { path: "/leaderboard", component: LeaderboardPage, name: "Leaderboard" },
+  { path: "/:notFound", component: NotFound, name: "404" },
+];
+
+export const RoutesEnum = {
+  Schedule: "/schedule",
+  Leaderboard: "/leaderboard",
+  NotFound: '/404'
+};
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
