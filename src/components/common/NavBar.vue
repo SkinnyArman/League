71bc1c2c9b20ab87fd9816002aa44fcebf4b9def
Arman Amini <armanwithamini@gmail.com>
2024-05-10 15:57:11 -0700

    fix: :pencil2: use the nav tag for navbar instead of div

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="bg-blue flex items-center h-15 px-12 justify-between">
-    <img src="images/logo.svg" />
+  <nav class="bg-blue flex items-center h-15 px-12 justify-between">
+    <img src="images/logo.svg" alt="logo"/>
     <div class="flex">
       <RouterLink
         class="inline-flex items-center text-white mr-10 text-body1"
         :to="RoutesEnum.Schedule"
       >
-        <img src="images/schedule.png" class="h-6 mr-1" />
+        <img src="images/schedule.png" class="h-6 mr-1" alt="schedule" />
         Schedule
       </RouterLink>
       <RouterLink
         class="inline-flex items-center text-white text-body1"
         :to="RoutesEnum.Leaderboard"
       >
-        <img src="images/leaderboard.png" class="h-6 mr-1" />
+        <img src="images/leaderboard.png" class="h-6 mr-1" alt="leaderbord"/>
         Leaderboard
       </RouterLink>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script setup>
